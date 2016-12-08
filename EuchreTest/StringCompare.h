4828cb7e01b63bb39fe743ab30bd52fd6deb1c29
tylerjosephrose <tylerjosephrose@gmail.com>
2016-12-08 10:45:37 -0500

    Adding StringCompare function to Repo

---
+++ EuchreTest/StringCompare.h
@@ -0,0 +1,23 @@
+#ifndef STRINGCOMPARE_h
+#define STRINGCOMPARE_h
+
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+bool icompare(const string &a, const string &b)
+{
+    if (b.size() != a.size())
+        return false;
+
+    for (unsigned int i = 0; i < a.size(); ++i)
+    {
+        if (tolower(a[i]) != tolower(b[i]))
+            return false;
+    }
+    return true;
+};
+
+
+#endif
